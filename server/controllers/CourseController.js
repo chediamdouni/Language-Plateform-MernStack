c28d81b7df7834f26d11e15144c386b3edc7a8dd
Chedi.Amdouni <chedi.amdouni@esprit.tn>
2024-02-13 14:15:13 +0100

    Correction d'une bogue dans CourController

--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -82,111 +82,120 @@ const getCourseById = async (req, res) => {
   }
 };
 
-
 // inscription a un seul cour
 const registerSingleCourse = async (req, res) => {
-    const { courseId } = req.body;
-    const userId = req.user._id;
-    try {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        return res.status(404).json({ message: "Course not found" });
-      }
+  const { courseId } = req.body;
+  const userId = req.user._id;
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    if (!course.etudiantsInscrits.includes(userId)) {
+      return res.status(400).json({ message: "étudiant n'est pas trouvé" });
+    }
+    course.etudiantsInscrits.push(userId);
+    await course.save();
+    res.status(200).json({ message: "inscription au cours réussie" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+// inscription a plusieurs cours
+const registerMultipleCourses = async (req, res) => {
+  const { coursesId } = req.body;
+  const userId = req.user._id;
+  try {
+    const courses = await Course.find({ _id: { $in: coursesId } });
+    const inscrits = [];
+
+    for (const course of courses) {
       if (!course.etudiantsInscrits.includes(userId)) {
-        return res.status(400).json({ message: "étudiant n'est pas trouvé" });
+        course.etudiantsInscrits.push(userId);
+        await course.save();
+        inscrits.push(course);
       }
-      course.etudiantsInscrits.push(userId);
-      await course.save();
-      res.status(200).json({ message: "inscription au cours réussie" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
     }
-  };
-  // inscription a plusieurs cours
-  const registerMultipleCourses = async (req, res) => {
-    const { coursesId } = req.body;
-    const userId = req.user._id;
-    try {
-      const courses = await Course.find({ _id: { $in: coursesId } });
-      const inscrits = [];
-  
-      for (const course of courses) {
-        if (!course.etudiantsInscrits.includes(userId)) {
-          course.etudiantsInscrits.push(userId);
-          await course.save();
-          inscrits.push(course);
-        }
-      }
-      res.status(200).json(inscrits);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: "l'inscription à plusieurs cours n'a pas ete éffectuer",
-      });
+    res.status(200).json(inscrits);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "l'inscription à plusieurs cours n'a pas ete éffectuer",
+    });
+  }
+};
+
+// Desincription d'un cours
+const unregisterCourse = async (req, res) => {
+  const { courseId } = req.body;
+  const userId = req.user._id;
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({ message: "Cours non trouvé" });
     }
-  };
-  
-  // Desincription d'un cours
-  const unregisterCourse = async (req, res) => {
-    const { courseId } = req.body;
-    const userId = req.user._id;
-    try {
-      const course = await Course.findById(courseId);
-      if (!course) {
-        return res.status(404).json({ message: "Cours non trouvé" });
-      }
-  
-      if (!course.etudiantsInscrits.includes(userId)) {
-        return res
-          .status(400)
-          .json({ message: "Vous n'êtes pas inscrit à ce cours" });
-      }
-  
-      course.etudiantsInscrits = course.etudiantsInscrits.filter(
-        (id) => id !== userId
-      );
-      await course.save();
-  
-      res.status(200).json({ message: "Désinscription du cours réussie" });
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: "la desincription du cour n'a pas ete effectuer " });
+
+    if (!course.etudiantsInscrits.includes(userId)) {
+      return res
+        .status(400)
+        .json({ message: "Vous n'êtes pas inscrit à ce cours" });
     }
-  };
-  // consulter les cours auxquels les apprenants sont inscrits
-  const registredCourses = async (req, res) => {
-    const userId = req.params.userId;
-    try {
-      const user = await User.findById(userId).populate("etudiantsInscrits");
-      if (!user) {
-        return res.status(404).json({ message: "User n'est pas trouvé" });
-      }
-      res.status(200).json({ coursInscrits: user.etudiantsInscrits });
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: "il y a un probleme dans les cours enregistrés " });
+
+    course.etudiantsInscrits = course.etudiantsInscrits.filter(
+      (id) => id !== userId
+    );
+    await course.save();
+
+    res.status(200).json({ message: "Désinscription du cours réussie" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "la desincription du cour n'a pas ete effectuer " });
+  }
+};
+// consulter les cours auxquels les apprenants sont inscrits
+const registredCourses = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId).populate("etudiantsInscrits");
+    if (!user) {
+      return res.status(404).json({ message: "User n'est pas trouvé" });
     }
-  };
-  
-  // consulter les apprenants inscrits a un cours
-  const consulterEtudiantsInscrits = async (req, res) => {
-    const courseId = req.params.courseId;
-    try {
-      const course = await Course.findById(courseId).populate(etudiantsInscrits);
-      if (!course) {
-        return res.status(404).json({ message: "le cours est introuvable" });
-      }
-      res.status(200).json({ etudiantsInscrits: course.etudiantsInscrits });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: "Erreur consulter les apprenants inscrits a un cours ",
-      });
+    res.status(200).json({ coursInscrits: user.etudiantsInscrits });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "il y a un probleme dans les cours enregistrés " });
+  }
+};
+
+// consulter les apprenants inscrits a un cours
+const consulterEtudiantsInscrits = async (req, res) => {
+  const courseId = req.params.courseId;
+  try {
+    const course = await Course.findById(courseId).populate(etudiantsInscrits);
+    if (!course) {
+      return res.status(404).json({ message: "le cours est introuvable" });
     }
-  };
-  
+    res.status(200).json({ etudiantsInscrits: course.etudiantsInscrits });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Erreur consulter les apprenants inscrits a un cours ",
+    });
+  }
+};
+
+module.exports = {
+  AddCourse,
+  updateCourse,
+  registredCourses,
+  registerMultipleCourses,
+  registerSingleCourse,
+  deleteCourse,
+  getCourseById,
+  getAllCourse,
+};
