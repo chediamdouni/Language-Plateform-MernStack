f7f70362a6f1a3f7e24630e0de16e0f6fe868dd2
Chedi.Amdouni <chedi.amdouni@esprit.tn>
2024-04-14 17:22:03 +0200

    AddTodoModal.tsx sert d'interface aux utilisateurs pour ajouter de nouveaux éléments "Todo"
    AddTodoModal.tsx est un autre élément clé de votre application de calendrier qui permet aux utilisateurs d'ajouter de nouveaux éléments "Todo".
    EventInfo.tsx est un élément essentiel de votre application de calendrier. Il donne des renseignements sur les événements

---
+++ client/src/components/Calendar/EventInfo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { IEventInfo } from "./EventCalendar";
+
+interface IProps {
+  event: IEventInfo;
+}
+
+const EventInfo = ({ event }: IProps) => {
+  return (
+    <>
+      <Typography>{event.description}</Typography>
+    </>
+  );
+};
+
+export default EventInfo;
